5c82d60a42f49b5cfd72f8f241b29087600638a2
Shiv Shankar <imshankar369@gmail.com>
2026-07-27 19:37:37 +0600

    Add Database class with connect and save methods

---
+++ src/database/Database.java
@@ -0,0 +1,17 @@
+package database;
+
+public class Database {
+
+    public void connect() {
+
+        System.out.println("Database Connected Successfully");
+
+    }
+
+    public void save(String data) {
+
+        System.out.println("Saved : " + data);
+
+    }
+
+}
