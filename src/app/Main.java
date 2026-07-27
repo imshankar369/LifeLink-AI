714f08038dd19e69a231088376eeb7df27ec9c77
Shiv Shankar <imshankar369@gmail.com>
2026-07-27 19:40:43 +0600

    Enhance Main class with AI features and data display
    Added driver and vehicle initialization, and integrated AI monitoring and accident prediction.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,57 @@
 package app;
 
+import ai.AccidentPredictionAI;
+import ai.DriverMonitoringAI;
+
+import model.Driver;
+import model.Vehicle;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to LifeLink AI");
+
+        Driver driver =
+                new Driver(
+                        "D001",
+                        "Shibshankar Mondal",
+                        22,
+                        "01700000000",
+                        "O+",
+                        "DL-1001",
+                        4,
+                        true
+                );
+
+        Vehicle vehicle =
+                new Vehicle(
+                        "V001",
+                        "DHK-12345",
+                        "Car",
+                        110,
+                        23.8103,
+                        90.4125,
+                        "Lane-2",
+                        true
+                );
+
+        driver.displayInfo();
+
+        System.out.println();
+
+        vehicle.displayVehicle();
+
+        System.out.println();
+
+        DriverMonitoringAI monitor =
+                new DriverMonitoringAI();
+
+        monitor.monitorDriver(driver);
+
+        System.out.println();
+
+        AccidentPredictionAI ai =
+                new AccidentPredictionAI(vehicle, driver);
+
+        ai.analyze();
     }
 }
