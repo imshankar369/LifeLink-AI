74b037f42be9a4c0a4e8604ea5038189df102947
Shiv Shankar <imshankar369@gmail.com>
2026-07-27 19:36:55 +0600

    Add Constants class with speed and project name

---
+++ src/util/Constants.java
@@ -0,0 +1,12 @@
+package util;
+
+public class Constants {
+
+    public static final double MAX_SPEED = 120.0;
+
+    public static final double SAFE_SPEED = 60.0;
+
+    public static final String PROJECT_NAME =
+            "LifeLink AI";
+
+}
