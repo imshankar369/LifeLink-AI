2b0f529c11a49af017b7b6dae81d2e4335749df1
Shiv Shankar <imshankar369@gmail.com>
2026-07-27 19:35:25 +0600

    Add AmbulanceService class for dispatching ambulances

---
+++ src/service/AmbulanceService.java
@@ -0,0 +1,13 @@
+package service;
+
+import model.Ambulance;
+
+public class AmbulanceService {
+
+    public void dispatchAmbulance(Ambulance ambulance) {
+
+        ambulance.dispatch();
+
+    }
+
+}
